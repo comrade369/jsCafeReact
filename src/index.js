f5c202eae3ba4419734bd05084802c1ef84d527a
comrade369 <saveallchildreninworld@gmail.com>
2023-11-24 20:04:17 +0530

    4 Modules Completed

--- src/index.js
+++ src/index.js
@@ -1,16 +1,53 @@
+// learning react elements, functional components, conditional rendering, fragments, javascript in jsx ...etc.
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// default javascript 
-const JSP = React.createElement("p", {className: "text-div"}, "Hello, from javascript");
+// Element Rendering.
+const reactElement0 = <>This is a React Element</>;
+// root.render(reactElement0);
 
-// default jsx (javascript xml)
-const JSXP = <p>Hello, from jsx</p>;
+// Functional Component Rendering.
+const FunctionalComponent = () => {
+    return (
+        <>
+            This is a Functional Component 
+        </>
+    );
+}
+// root.render(FunctionalComponent());
 
-const JSDiv = React.createElement("div", {}, [JSP, JSXP]);
+// Conditional Rendering.
+const isMorning = false;
+const reactElement1 = <p>It is morning,<strong> react Element</strong></p>;
+const reactElement2 = <p>It is Night,<strong> Another react Element</strong></p>
+const conditionalRender = isMorning ? reactElement1 : reactElement2;
+// root.render(conditionalRender);
 
-root.render(JSDiv);
+// Javascript in jsx.
+const myName = window.prompt("Enter your name:");
+const reactElement = <h1>Your name is {myName}, we write javascript in jsx using curly brackets.</h1>
+// root.render(reactElement);
 
-// npm run serve -- to run app
+// Attributes in jsx.
+const attribute = <button onClick={() => handleClick(myName)}>Click here to alert message.</button>
+const handleClick = (myName) => {
+    return window.alert("You Clicked button, your name is " + myName);
+}
+
+// All react elements in on functional component.
+function AllElementsComponents() {
+    return (
+        <>
+            {reactElement0}
+            {conditionalRender}
+            {reactElement}
+            {FunctionalComponent()} {" "}
+            {attribute}
+        </>
+    );
+}
+root.render(AllElementsComponents());
+
